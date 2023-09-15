9d818462f206d50e5abdb231facd1aa28148af8b
rommel dalisay <rommelmdalisay@gmail.com>
2023-09-15 09:09:38 +0800

    singlepost route

--- src/Routes.js
+++ src/Routes.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Create from './Create';
-
+import SinglePost from './SinglePost';
 const RoutedApp = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/create" element={<Create />} />
+                <Route path="/posts/:id" element={<SinglePost />} />
             </Routes>
         </BrowserRouter>
     )
