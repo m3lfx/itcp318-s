4263024e690ccb64d39f1894f4e15d632ab7ac36
rommel dalisay <rommelmdalisay@gmail.com>
2023-09-15 10:45:59 +0800

    update post

--- src/UpdatePost.js
+++ src/UpdatePost.js
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
 
 const UpdatePost = () => {
+    let { id, } = useParams()
+    const [post, setPost] = useState({})
+    useEffect(() => {
+        axios.get(`${process.env.REACT_APP_API}/posts/${id}/edit`)
+            .then(response => {
+                console.log(response);
+                setPost(response.data);
+            })
+            .catch(error => {
+                alert('Error fetching posts')
+                console.log(error)
+            });
+    }, [])
+    console.log(post)
   return (
     <div>UpdatePost</div>
   )
