6f60a50d086452d1aed7915c29c392feeca77a64
rommel dalisay <rommelmdalisay@gmail.com>
2023-09-15 10:56:11 +0800

    edit form

--- src/UpdatePost.js
+++ src/UpdatePost.js
@@ -1,10 +1,41 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import axios from 'axios'
+import Nav from './Nav'
+import Title from './Title'
 
 const UpdatePost = () => {
     let { id, } = useParams()
+    let navigate = useNavigate()
     const [post, setPost] = useState({})
+    const [state, setState] = useState({
+        title: '',
+        content: '',
+        user: ''
+    });
+
+    const { title, content, user } = state;
+
+    const handleChange = name => event => {
+        console.log('name', name, 'event', event.target.value);
+        setState({ ...state, [name]: event.target.value });
+    };
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        axios.put(`${process.env.REACT_APP_API}/posts/${post.id}`, { title, content, user, }).then(response => {
+                console.log(response);
+                setState({ ...state, title: '', content: '', user: '' });
+                // show sucess alert
+                // alert(`Post titled ${response.data.title} is created`);
+                return navigate("/");
+            })
+            .catch(error => {
+                console.log(error.response);
+                alert(error.response.data.error);
+            });
+    };
+
     useEffect(() => {
         axios.get(`${process.env.REACT_APP_API}/posts/${id}/edit`)
             .then(response => {
@@ -18,7 +49,44 @@ const UpdatePost = () => {
     }, [])
     console.log(post)
   return (
-    <div>UpdatePost</div>
+    <>
+    <Nav />
+    <div className="container p-5">
+        <Title title="CREATE POST" />
+
+        <br />
+        <form onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label className="text-muted">Title</label>
+                <input
+                    type="text" className="form-control" placeholder="Post title"
+                    required
+                    onChange={handleChange('title')}
+                    value={post.title}
+                />
+            </div>
+            <div className="form-group">
+                <label className="text-muted">Content</label>
+                <textarea
+                    type="text" className="form-control" placeholder="Write something.." required
+                    onChange={handleChange('content')}
+                    value={post.content}
+                />
+            </div>
+            <div className="form-group">
+                <label className="text-muted">User</label>
+                <input
+                    type="text" className="form-control" placeholder="Your name"
+                    required
+                    onChange={handleChange('user')} 
+                    value={post.user_id}/>
+            </div>
+            <div>
+                <button className="btn btn-primary">Create</button>
+            </div>
+        </form>
+    </div>
+</>
   )
 }
 
