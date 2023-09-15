4060cf065123eda99364834edd5f23ca9c2c0426
rommel dalisay <rommelmdalisay@gmail.com>
2023-09-15 09:20:50 +0800

    singlepost route

--- src/SinglePost.js
+++ src/SinglePost.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
 
 const SinglePost = () => {
+    let { id, } = useParams()
+    const [post, setPost] = useState({})
+    console.log(id)
+
+    useEffect(() => {
+        axios.get(`${process.env.REACT_APP_API}/posts/${id}`)
+      .then(response => {
+        console.log(response);
+        // setPost(response.data);
+      })
+      .catch(error => {
+        alert('Error fetching posts')
+        console.log(error)
+      });
+      }, [])
   return (
     <div>SinglePost</div>
   )
