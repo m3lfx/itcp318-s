55dedd299b2191def23a90cd8948f488b6d0d0d9
rommel dalisay <rommelmdalisay@gmail.com>
2023-09-22 09:42:31 +0800

    session storage

--- src/Login.js
+++ src/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
@@ -7,12 +8,14 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import Button from '@mui/material/Button';
 import axios from 'axios';
+import { authenticate } from './helpers';
 
 const Login = ({ open, handleClose }) => {
     const [state, setState] = useState({
         email: '',
         password: '',
     });
+    const navigate = useNavigate()
 
     const { email, password } = state;
     const handleChange = name => event => {
@@ -24,6 +27,7 @@ const Login = ({ open, handleClose }) => {
         event.preventDefault();
         axios.post(`${process.env.REACT_APP_API}/login`, { email, password }).then(response => {
             console.log(response);
+            authenticate(response, () => navigate("/create"));
             // setState({ ...state, title: '', content: '', user: '' });
             // return navigate("/");
         })
