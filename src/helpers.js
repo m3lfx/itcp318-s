274d32c993c2cdb81f2981ac9bf972032296b9f3
rommel dalisay <rommelmdalisay@gmail.com>
2023-09-22 09:38:37 +0800

    helper functions

--- src/helpers.js
+++ src/helpers.js
@@ -2,8 +2,8 @@
 export const authenticate = (response, next) => {
     if (window !== 'undefined') {
         // console.log('authenticate', response)
-        sessionStorage.setItem('token', JSON.stringify(response.data.token));
-        sessionStorage.setItem('user', JSON.stringify(response.data.name));
+        sessionStorage.setItem('token', JSON.stringify(response.data.access_token));
+        sessionStorage.setItem('user', JSON.stringify(response.data.user.name));
     }
     next();
 };
